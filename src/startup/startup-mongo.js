b90ccb1cdf583fa3c8727fc9d52874ebe4526a04
fedacking <fedacking@gmail.com>
2021-10-15 04:36:05 -0300

    Fix to variables

--- src/startup/startup-mongo.js
+++ src/startup/startup-mongo.js
@@ -6,8 +6,8 @@ const config = require('../../config/config');
 
 console.log("Reading Config"); // Debugeando JS + Heroku
 const { host, port } = config.db;
-let uri = `mongodb://${host}:${port}/app`;
-let uri = "mongodb+srv://ubademy-g2:" + process.env.MONGO_DB_PASS + "@auth-server.i7qbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let uri = `mongodb://${host}:${port}/app`
+uri = "mongodb+srv://ubademy-g2:" + process.env.MONGO_DB_PASS + "@auth-server.i7qbi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 module.exports = () => {
 	mongoose.connect(uri, {
