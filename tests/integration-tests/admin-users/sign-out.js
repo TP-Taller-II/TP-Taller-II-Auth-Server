6547e615e57887f933e73a5d118ceb3b9023f548
Rodrigo Souto <rnsoutob@gmail.com>
2021-10-22 15:55:30 -0300

    feat(adminuser): testing signin and signout

---
+++ tests/integration-tests/admin-users/sign-out.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const assert = require('assert');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sandbox = require('sinon').createSandbox();
+
+const { TokenServices } = require('../../../src/services');
+const Model = require('../../../src/databases/mongodb/model');
+const STATUS_CODES = require('../../../src/utils/status-codes.json');
+
+process.env.PORT = 3030;
+
+const app = require('../../../src');
+
+chai.use(chaiHttp);
+
+describe('AdminUsers', async () => {
+
+	beforeEach(() => {
+		process.env.PORT = 3030;
+		sandbox.stub(TokenServices.prototype, 'verifyToken').resolves({ _id: '60456ebb0190bf001f6bbee2' });
+	});
+
+	afterEach(() => {
+		delete process.env.PORT;
+		sandbox.restore();
+	});
+
+
+	describe('Sign out', async () => {
+
+		it('Should set status code 200 when it removes the token', async () => {
+
+			sandbox.stub(Model.prototype, 'update').resolves(true);
+
+			const res = await chai.request(app).post('/auth-server/v1/admin/signOut')
+				.set('x-auth-token', 'some-valid-token');
+
+			assert.deepStrictEqual(res.status, STATUS_CODES.OK);
+			assert.deepStrictEqual(res.body, { message: 'User sign out' });
+
+			sandbox.assert.calledOnceWithExactly(Model.prototype.update, { _id: '60456ebb0190bf001f6bbee2' }, { accessToken: null });
+		});
+
+		it('Should set status code 500 when database fails', async () => {
+
+			sandbox.stub(Model.prototype, 'update').rejects(new Error('DB ERROR'));
+
+			const res = await chai.request(app).post('/auth-server/v1/admin/signOut')
+				.set('x-auth-token', 'some-valid-token');
+			assert.deepStrictEqual(res.status, STATUS_CODES.INTERNAL_SERVER_ERROR);
+			assert.deepStrictEqual(res.body, { message: 'DB ERROR' });
+
+			sandbox.assert.calledOnce(Model.prototype.update);
+		});
+	});
+});
